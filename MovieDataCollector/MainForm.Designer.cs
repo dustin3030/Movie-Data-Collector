663ce37d98bfd54c1e0417dc5798752f55517663
dustin3030 <dustin.oldroyd@gmail.com>
2017-01-30 10:09:09 -0700

    Design of main form

---
+++ MovieDataCollector/MainForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace MovieDataCollector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tVShowsInstructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.moviesInstructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compatibilityCheckerInstructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iMDBcomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.theTVDBcomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.theMovieDBorgToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tvButton = new System.Windows.Forms.Button();
+            this.movieButton = new System.Windows.Forms.Button();
+            this.CompatibilityCheckerButton = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.GreenYellow;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.goToToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(229, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tVShowsInstructionsToolStripMenuItem,
+            this.moviesInstructionsToolStripMenuItem,
+            this.compatibilityCheckerInstructionsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 19);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // goToToolStripMenuItem
+            // 
+            this.goToToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iMDBcomToolStripMenuItem,
+            this.theTVDBcomToolStripMenuItem,
+            this.theMovieDBorgToolStripMenuItem});
+            this.goToToolStripMenuItem.Name = "goToToolStripMenuItem";
+            this.goToToolStripMenuItem.Size = new System.Drawing.Size(51, 19);
+            this.goToToolStripMenuItem.Text = "&Go To";
+            // 
+            // tVShowsInstructionsToolStripMenuItem
+            // 
+            this.tVShowsInstructionsToolStripMenuItem.Name = "tVShowsInstructionsToolStripMenuItem";
+            this.tVShowsInstructionsToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.tVShowsInstructionsToolStripMenuItem.Text = "TV Shows Instructions";
+            // 
+            // moviesInstructionsToolStripMenuItem
+            // 
+            this.moviesInstructionsToolStripMenuItem.Name = "moviesInstructionsToolStripMenuItem";
+            this.moviesInstructionsToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.moviesInstructionsToolStripMenuItem.Text = "Movies Instructions";
+            // 
+            // compatibilityCheckerInstructionsToolStripMenuItem
+            // 
+            this.compatibilityCheckerInstructionsToolStripMenuItem.Name = "compatibilityCheckerInstructionsToolStripMenuItem";
+            this.compatibilityCheckerInstructionsToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.compatibilityCheckerInstructionsToolStripMenuItem.Text = "Compatibility Checker Instructions";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(265, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // iMDBcomToolStripMenuItem
+            // 
+            this.iMDBcomToolStripMenuItem.Name = "iMDBcomToolStripMenuItem";
+            this.iMDBcomToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.iMDBcomToolStripMenuItem.Text = "IMDB.com";
+            // 
+            // theTVDBcomToolStripMenuItem
+            // 
+            this.theTVDBcomToolStripMenuItem.Name = "theTVDBcomToolStripMenuItem";
+            this.theTVDBcomToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.theTVDBcomToolStripMenuItem.Text = "TheTVDB.com";
+            // 
+            // theMovieDBorgToolStripMenuItem
+            // 
+            this.theMovieDBorgToolStripMenuItem.Name = "theMovieDBorgToolStripMenuItem";
+            this.theMovieDBorgToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.theMovieDBorgToolStripMenuItem.Text = "TheMovieDB.org";
+            // 
+            // tvButton
+            // 
+            this.tvButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.tvButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tvButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.tvButton.Location = new System.Drawing.Point(15, 46);
+            this.tvButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tvButton.Name = "tvButton";
+            this.tvButton.Size = new System.Drawing.Size(200, 59);
+            this.tvButton.TabIndex = 1;
+            this.tvButton.Text = "&TV Shows";
+            this.tvButton.UseVisualStyleBackColor = false;
+            // 
+            // movieButton
+            // 
+            this.movieButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.movieButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.movieButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.movieButton.Location = new System.Drawing.Point(15, 117);
+            this.movieButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.movieButton.Name = "movieButton";
+            this.movieButton.Size = new System.Drawing.Size(200, 67);
+            this.movieButton.TabIndex = 1;
+            this.movieButton.Text = "&Movies";
+            this.movieButton.UseVisualStyleBackColor = false;
+            // 
+            // CompatibilityCheckerButton
+            // 
+            this.CompatibilityCheckerButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.CompatibilityCheckerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CompatibilityCheckerButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.CompatibilityCheckerButton.Location = new System.Drawing.Point(15, 196);
+            this.CompatibilityCheckerButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.CompatibilityCheckerButton.Name = "CompatibilityCheckerButton";
+            this.CompatibilityCheckerButton.Size = new System.Drawing.Size(200, 68);
+            this.CompatibilityCheckerButton.TabIndex = 1;
+            this.CompatibilityCheckerButton.Text = "&Compatibility Checker";
+            this.CompatibilityCheckerButton.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(229, 281);
+            this.Controls.Add(this.CompatibilityCheckerButton);
+            this.Controls.Add(this.movieButton);
+            this.Controls.Add(this.tvButton);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tVShowsInstructionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem moviesInstructionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compatibilityCheckerInstructionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iMDBcomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem theTVDBcomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem theMovieDBorgToolStripMenuItem;
+        private System.Windows.Forms.Button tvButton;
+        private System.Windows.Forms.Button movieButton;
+        private System.Windows.Forms.Button CompatibilityCheckerButton;
+    }
+}
+
