4305e94958ef7371736aaf2ca1c42d6ffca295f8
dustin3030 <dustin.oldroyd@gmail.com>
2017-02-14 13:33:33 -0700

    Working on conversion form

--- MovieDataCollector/ConversionForm.cs
+++ MovieDataCollector/ConversionForm.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics; //Allows for using Process.Start codes lines
+using System.Drawing;
+using System.IO; //allows for file manipulation
+using System.Text;
+using System.Net.Mail; // For Notification
+using MediaInfoNET; /* http://teejeetech.blogspot.com/2013/01/mediainfo-wrapper-for-net-projects.html Copyright (c) 2013 Tony George (teejee2008@gmail.com)
+                      GNU General Public License version 2.0 (GPLv2)
+                      Downloaded Wrapper for returning media info from files.
+                      Need to have both the wrapper (MediaInfoNet.dll) and the DLL (MediaInfo.dll) saved in the
+                      Application folder (Release or Debug) or it will not work*/
 
 namespace MovieDataCollector
 {
